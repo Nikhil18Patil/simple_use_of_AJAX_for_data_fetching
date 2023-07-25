609210931382b69450a9538dc0aa2df6b8f6d780
Nikhil18Patil <nikhilpatil18012004@gmail.com>
2023-07-25 13:41:06 +0530

    Your commit message here

--- static/script.js
+++ static/script.js
@@ -14,6 +14,30 @@ document.getElementById("fetchDataBtn").addEventListener("click", function() {
     });
 });
 
+document.getElementById("fetchSpecificDataBtn").addEventListener("click", function() {
+    // Get the input name from the HTML and remove leading/trailing spaces
+    var inputName = document.getElementById("nameInput").value.trim();
+
+    // Send an AJAX request to fetch the data for the specific name
+    $.ajax({
+        type: "GET",
+        url: "/fetch_data/",
+        dataType: "json",
+        success: function(responseData) {
+            // Filter the data by the input name (case insensitive)
+            var specificNameData = responseData.filter(function(item) {
+                return item.name.toLowerCase() === inputName.toLowerCase();
+            });
+
+            // Display the filtered data on the web page
+            displayData(specificNameData);
+        },
+        error: function(xhr, status, error) {
+            console.error("Request failed. Status: " + xhr.status);
+        }
+    });
+});
+
 // Function to display the fetched data on the web page
 function displayData(data) {
     var dataContainer = document.getElementById("dataContainer");
